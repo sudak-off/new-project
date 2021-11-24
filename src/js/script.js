133460dc952302531df12561755dc4610f300e55
Artyom <krestonos@gmail.com>
2021-11-24 12:11:08 +0200

    new project

---
+++ src/js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+
+//let number = 5;
+//console.log(number);
+//========================================
+//let Admin;
+//let yellow;
+
+//yellow = "Djoni";
+//Admin = yellow;
+
+//console.log(Admin);
+//=========================================
+//const obj = {
+//    a: 50
+//};
+//obj.a = 10;
+//console.log(obj);
+//=========================================
+//let name = "Ilya";
+
+//console.log( `hello ${1}` ); 
+
+//console.log( `hello ${"name"}` ); 
+
+//console.log( `hello ${name}` );
+//=========================================
+//console.log(red);
+
+
+const answer = [];
+
+answer[0] = prompt('kak vashe imya?', '' );
+answer[1] = prompt('kak vashe imy111a?', '' );
+answer[2] = prompt('kak vashe imy222a?', '' );
+
+
+
+document.write(answer);
